c2bd824a71381068832f99cc3eee79a4880dbd31
dk5042 <damianknopek03@gmail.com>
2023-05-29 19:03:43 +0200

    klasa player

--- SFML test projekt/Fish.cpp
+++
@@ -1,8 +0,0 @@
-#include "Fish.h"
-
-Fish::Fish(float width, float height, float x, float y)
-{
-	hit_box.setPosition(x, y);
-	hit_box.setSize(Vector2<float> {width, height});
-}
-
---
+++ SFML test projekt/Player.cpp
@@ -0,0 +1,13 @@
+#include "Player.h"
+
+
+
+void Player::move()
+{
+
+}
+
+void Player::draw(RenderTarget& target, RenderStates states)
+{
+	target.draw(fish_body);
+}
